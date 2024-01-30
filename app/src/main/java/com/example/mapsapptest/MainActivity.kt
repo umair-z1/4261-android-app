b5dcdb0a10073ef129dfdb3bb943ec61b203c2f2
Umair <uamin6@gatech.edu>
2024-01-30 01:01:41 -0500

    Removed unused imports

--- app/src/main/java/com/example/mapsapptest/MainActivity.kt
+++ app/src/main/java/com/example/mapsapptest/MainActivity.kt
@@ -4,10 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
